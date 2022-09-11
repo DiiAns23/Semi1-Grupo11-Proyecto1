ae09fcfa4dcedaa6a93f5aea48278754ff7afd8d
Iovana Miranda <iovaclioma@gmail.com>
2022-09-11 10:05:32 -0600

    Se verificó contraseñas en el registro

--- Frontend/src/pages/login.jsx
+++ Frontend/src/pages/login.jsx
@@ -50,7 +50,7 @@ export default function LoginUsuario() {
                     `${error}!`,
                     // ``,
                     `error`
-                  )
+                )
             })
     }
 
