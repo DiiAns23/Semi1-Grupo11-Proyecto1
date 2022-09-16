1cece90cb8ed052cda168ac80e65f50ae2ae4dcd
edpo1998 <edpo11081998@gmail.com>
2022-09-15 23:19:04 -0600

    ADDED: Changes New

---
+++ BackendPython/server/views/student/student_getNotFriend.py
@@ -0,0 +1,69 @@
+import numpy as np
+from flask.views import MethodView
+from flask import request,json,jsonify
+from server.views.utils.templateresponse import Response
+from server import mysql
+import hashlib
+
+class StudentGetNotFriends(MethodView):
+    def get(self):
+        request_data = request.get_json()
+        # Data
+        id_usuario = request_data.get('id_usuario',None)
+        response = Response()
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute('''call getUsers({});'''.format(id_usuario))
+            row_headers=[x[0] for x in cursor.description]
+            data = cursor.fetchall()
+            users_data=[]
+            for result in data:
+                    users_data.append(dict(zip(row_headers,result)))
+            
+            cursor2 = mysql.connection.cursor()
+            cursor2.execute('''call getFriends({});'''.format(id_usuario))
+            row_headers=[x[0] for x in cursor2.description]
+            data = cursor2.fetchall()
+            friends_data=[]
+            for result in data:
+                friends_data.append(dict(zip(row_headers,result)))
+
+            for i,user in enumerate(users_data):
+                for j,friend in enumerate(friends_data):
+                    if(user['id_usuario'] == friend['id_usuario_f'] or user['id_usuario'] == friend['id_friend_f']):
+                        users_data = users_data[i:1]
+                        
+            return response.Succesfully(users_data)
+        except mysql.connector.Error as err:
+            return response.Bad_Request(err)
+
+    def post(self):
+        request_data = request.get_json()
+        # Data
+        id_usuario = request_data.get('id_usuario',None)
+        response = Response()
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute('''call getRequestF({});'''.format(id_usuario))
+            row_headers=[x[0] for x in cursor.description]
+            data = cursor.fetchall()
+            json_data=[]
+            for result in data:
+                    json_data.append(dict(zip(row_headers,result)))
+            
+            id_friends = []
+            for user in json_data:
+                if id_usuario != user.id_usuario_f:
+                    id_friends.append(user.id_usuario_f)
+                else:
+                    id_friends.append(user.id_friend_f)
+            data_response = {}
+
+            for friend in id_friends:
+                cursor2 = mysql.connection.cursor()
+                cursor2.execute('''call getNames({});'''.format(friend))
+                data_response[friend] = cursor2.fetchall()[0]
+
+            return response.Succesfully(data_response)
+        except mysql.connector.Error as err:
+            return response.Bad_Request(err)
--- BackendPython/server/views/student/student_register.py
+++ BackendPython/server/views/student/student_register.py
@@ -7,13 +7,18 @@ import hashlib
 
 class StudentRegister(MethodView):
     def get(self):
-        request_data = request.get_json()
-        id = request_data.get('id',None)
-        cursor = mysql.connection.cursor()
-        cursor.execute(''' SELECT * FROM USUARIO WHERE id_usuario={}'''.format(id))
-        data = cursor.fetchall()
         response = Response()
-        return response.Succesfully(data)
+        try:
+            cursor = mysql.connection.cursor()
+            cursor.execute(''' SELECT * FROM USUARIO;''')
+            row_headers=[x[0] for x in cursor.description]
+            data = cursor.fetchall()
+            json_data=[]
+            for result in data:
+                    json_data.append(dict(zip(row_headers,result)))                    
+            return response.Succesfully(json_data)
+        except mysql.connector.Error as err:
+            return response.Bad_Request(err)
 
     def post(self):
         request_data = request.get_json()
@@ -22,14 +27,19 @@ class StudentRegister(MethodView):
         password_str = request_data.get('password',None)
         password_sha1 = hashlib.sha1(str.encode(password_str)).hexdigest()
         user = request_data.get('user',None)
-        photo = request_data.get('photo',None)
+        photo = request_data.get('photo','none')
+        ext = request_data.get('ext',None)
         response = Response()
         # Send Data
         try:
             cursor = mysql.connection.cursor()
             cursor.callproc('newUser',(email,password_sha1,user,photo))
+            row_headers=[x[0] for x in cursor.description]
             data = cursor.fetchall()
-            return response.Succesfully(data)
+            json_data=[]
+            for result in data:
+                    json_data.append(dict(zip(row_headers,result)))
+            return response.Succesfully(json_data)
         except mysql.connector.Error as err:
             return response.Bad_Request(err)
         
